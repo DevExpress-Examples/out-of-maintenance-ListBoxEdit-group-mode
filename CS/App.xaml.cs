b3545be4b221af973795bc93fb69204b5aea14e9
Sameishchev Anton <anton.sameishchev@devexpress.com>
2018-11-12 14:06:20 +0300

    files added

---
+++ CS/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DXGridSample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App: Application {
+        public App() {
+            ApplicationThemeHelper.UseLegacyDefaultTheme = false;
+        }
+    }
+}
